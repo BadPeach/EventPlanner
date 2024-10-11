21fcaab87f5ae744dc552f78c9a9a2e377a13da3
Apetroaei Cezar-Stefan <cezar-stefan.apetroaei@student.tuiasi.ro>
2024-10-11 13:03:28 +0300

    Adaugare ruta catre pagina de gestionare a evenimentelor

--- EventPlannerWeb/src/app/app.routes.ts
+++ EventPlannerWeb/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GestionareEvenimenteComponent } from './evenimente/gestionare-evenimente/gestionare-evenimente.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'gestionare-evenimente' , component:GestionareEvenimenteComponent}  
+];
